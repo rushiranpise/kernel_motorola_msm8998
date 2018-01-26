6cd81d002161053cb08de78c79cc28720c9709ca
Dan Pasanen <dan.pasanen@gmail.com>
2018-01-25 21:23:10 -0600

    greybus: build with clang if the kernel is using clang
    Change-Id: I84531fa1c3245b4a803387cd27f8be23ba3c3e9c

--- drivers/misc/motomods/greybus/Android.mk
+++ drivers/misc/motomods/greybus/Android.mk
@@ -100,12 +100,36 @@ else
 GB_KDIRARG := KERNELDIR="${ANDROID_PRODUCT_OUT}/obj/KERNEL_OBJ"
 endif
 
+ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
+ifneq ($(TARGET_KERNEL_CLANG_VERSION),)
+    # Find the clang-* directory containing the specified version
+    KERNEL_CLANG_VERSION := $(shell find $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/ -name AndroidVersion.txt -exec grep -l $(TARGET_KERNEL_CLANG_VERSION) "{}" \; | sed -e 's|/AndroidVersion.txt$$||g;s|^.*/||g')
+else
+    # Only set the latest version of clang if TARGET_KERNEL_CLANG_VERSION hasn't been set by the device config
+    KERNEL_CLANG_VERSION := $(shell ls -d $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/clang-* | xargs -n 1 basename | tail -1)
+endif
+TARGET_KERNEL_CLANG_PATH ?= $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/$(KERNEL_CLANG_VERSION)/bin
+ifeq ($(KERNEL_ARCH),arm64)
+    KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=aarch64-linux-gnu-
+else ifeq ($(KERNEL_ARCH),arm)
+    KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=arm-linux-gnu-
+else ifeq ($(KERNEL_ARCH),x86)
+    KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=x86_64-linux-gnu-
+endif
+
+KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(KERNEL_TOOLCHAIN_PATH)"
+KERNEL_CC ?= CC="$(ccache) $(TARGET_KERNEL_CLANG_PATH)/clang"
+
+GB_KERNEL_TOOLS_PREFIX := $(KERNEL_TOOLCHAIN_PATH)
+
+else
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(strip $(TARGET_KERNEL_CROSS_COMPILE_PREFIX))
 ifeq ($(TARGET_KERNEL_CROSS_COMPILE_PREFIX),)
 GB_KERNEL_TOOLS_PREFIX := arm-eabi-
 else
 GB_KERNEL_TOOLS_PREFIX := $(TARGET_KERNEL_CROSS_COMPILE_PREFIX)
 endif
+endif # TARGET_KERNEL_CLANG_COMPILE
 
 GB_ARCHARG := ARCH=$(TARGET_ARCH)
 ifeq ($(IGNORE_AUD_DEV_OUTLINE),true)
@@ -114,6 +138,7 @@ else
 GB_FLAGARG := EXTRA_CFLAGS+=-fno-pic
 endif
 GB_ARGS := $(GB_KDIRARG) $(GB_ARCHARG) $(GB_FLAGARG)
+GB_ARGS += $(KERNEL_CLANG_TRIPLE) $(KERNEL_CC)
 
 #Create vendor/lib/modules directory if it doesn't exist
 $(shell mkdir -p $(TARGET_OUT_VENDOR)/lib/modules)
